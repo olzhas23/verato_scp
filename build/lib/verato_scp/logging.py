06d3a7613d8581e920d8ee040623e22281eb7fde
Olzhas Shaikenov <olzhas.shaikenov@ARXMCMB-OSHAIKENOV.local>
2017-03-10 18:47:58 -0500

    .gitignore is now working

--- build/lib/verato_scp/logging.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-class Logger(object):
-    def __init__(self, filename="output.log"):
-        # self.terminal = sys.stdout
-        self.log = open(filename, "a")
-
-    def write(self, message):
-        # self.terminal.write(message)
-        self.log.write(message)
